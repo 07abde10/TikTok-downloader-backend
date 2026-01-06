36939af6e530e647f024f012165dff8f351930ba
abde <abdelwahab.keys@gmail.com>
2026-01-06 15:57:03 +0100

    Remove Instagram functionality - TikTok only

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,6 @@ Route::post('/tiktok/download', [TikTokController::class, 'download']);
 Route::post('/tiktok/download-file', [TikTokController::class, 'downloadFile']);
 Route::get('/tiktok/download-file', [TikTokController::class, 'downloadFile']);
 
-Route::post('/instagram/download', [InstagramController::class, 'download']);
-Route::post('/instagram/download-file', [InstagramController::class, 'downloadFile']);
-Route::get('/instagram/download-file', [InstagramController::class, 'downloadFile']);
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
